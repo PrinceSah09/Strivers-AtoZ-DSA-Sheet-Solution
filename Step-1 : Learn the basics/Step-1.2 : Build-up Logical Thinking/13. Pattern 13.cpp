2dda15de9374b4cd55568e0644560f2a4250ab1c
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-06-26 23:37:31 +0530

    Create 13. Pattern 13.cpp

---
+++ Step-1 : Learn the basics/Step-1.2 : Build-up Logical Thinking/13. Pattern 13.cpp
@@ -0,0 +1,26 @@
+/*
+Input: 5
+
+Output:
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+
+*/
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        int x=1;
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=i;j++){
+               cout<<x++<<" ";
+           }
+           cout<<endl;
+       }
+       
+    }
+};
