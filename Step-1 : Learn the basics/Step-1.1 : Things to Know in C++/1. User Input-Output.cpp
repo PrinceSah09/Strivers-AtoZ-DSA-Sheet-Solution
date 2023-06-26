b9fc5ec1a8fd5086befed70f1a654d427cb45226
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-06-26 21:31:50 +0530

    Update 1. User Input-Output.cpp

--- Step-1 : Learn the basics/Step-1.1 : Things to Know in C++/1. User Input-Output.cpp
+++ Step-1 : Learn the basics/Step-1.1 : Things to Know in C++/1. User Input-Output.cpp
@@ -1,1 +1,20 @@
+/*
+Question: Given two integers A and B. Your task is to return their product.  
+Example 1:
 
+Input:
+A = 1, B = 2
+Output:
+2
+Explanation:
+The multiplication of 1 and 2 is 2.  
+
+*/
+
+Solution: 
+class Solution{   
+public:
+    int multiplication(int A, int B){
+        return A*B; 
+    }
+};
