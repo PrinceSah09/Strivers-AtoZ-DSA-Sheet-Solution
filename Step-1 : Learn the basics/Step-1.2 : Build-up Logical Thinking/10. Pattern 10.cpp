5cbfa48b5079cbf2b3b10eb34f49e0e3183739c5
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-06-26 23:09:55 +0530

    Create 10. Pattern 10.cpp

---
+++ Step-1 : Learn the basics/Step-1.2 : Build-up Logical Thinking/10. Pattern 10.cpp
@@ -0,0 +1,37 @@
+/*
+
+Input: 5
+
+Output:
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+class Solution{
+public:
+    void printTriangle(int n) {
+        //Upper part
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+        
+        for(int i=1; i<=n-1;i++){
+            for(int j=1;j<=n-i;j++){
+                cout<<"* ";
+            }
+            cout<<endl;
+        }
+    }
+};
