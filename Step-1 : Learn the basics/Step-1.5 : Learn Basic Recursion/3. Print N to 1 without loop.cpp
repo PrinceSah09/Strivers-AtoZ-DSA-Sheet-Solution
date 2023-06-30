589f9f96f2dd8d2b275b194f2f6c102278be64c1
Prince Kumar <81484022+PrinceSah09@users.noreply.github.com>
2023-07-01 01:32:50 +0530

    Update 3. Print N to 1 without loop.cpp

--- Step-1 : Learn the basics/Step-1.5 : Learn Basic Recursion/3. Print N to 1 without loop.cpp
+++ Step-1 : Learn the basics/Step-1.5 : Learn Basic Recursion/3. Print N to 1 without loop.cpp
@@ -1,4 +1,18 @@
 // GFG
+
+/*
+Print numbers from N to 1 (space separated) without the help of loops.
+
+Example 1:
+
+Input:
+N = 10
+Output: 10 9 8 7 6 5 4 3 2 1
+
+*/
+
+
+//Time Complexity: O(N)
 class Solution {
   public:
     void printNos(int N) {
